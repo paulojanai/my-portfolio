d94ffac4a3f40745f3aa432b84917d858332ce66
pjanai <paulojanai.m.s@gmail.com>
2023-07-04 12:39:40 -0300

    feature: showing menu when scroll up

---
+++ js/scrollUpShowHeader.js
@@ -0,0 +1,28 @@
+function scrollUpShowHeader () {
+  let oldValue = 0;
+  var element = document.getElementById("header");
+
+  // element.classList.add("header-fixed");
+  window.addEventListener('scroll', function (e) {
+
+    // Get the new Value
+    newValue = window.pageYOffset;
+    // console.log(newValue);
+
+    // Subtract the two and conclude
+    if (oldValue - newValue < 0) {
+      // console.log("Up");
+      element.classList.add("show-header");
+    } else if (oldValue - newValue > 0) {
+      element.classList.remove("show-header");
+      // console.log("Down");
+    }
+
+    if (newValue == 0) {
+      return element.classList.remove("show-header");
+    }
+
+    // Update the old value
+    oldValue = newValue;
+  });
+}
